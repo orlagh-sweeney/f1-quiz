05d976b36ef0aa2ba768a3a6cfe671da59bccbb5
orlagh-sweeney <orlagh16@gmail.com>
2023-01-03 15:18:24 +0000

    Add const variables to scipt.js and add id names to li in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+const runQuizA = document.getElementsByClassName('btn-quiz-1');
+const runQuizB = document.getElementsByClassName('btn-quiz-2');
+const submitBtn = document.getElementsByClassName('submit');
+const nextBtn = document.getElementsByClassName('next');
+const quitBtn = document.getElementsByClassName('btn-quit');
+const quizName = document.getElementsByClassName('quiz-name');
+const questionText = document.getElementsByClassName('question');
+const userScore = document.getElementById('score');
+const userWrong = document.getElementById('incorrect');
+const userTimer = document.getElementById('time');
+
 function runQuiz() {
 
 }
