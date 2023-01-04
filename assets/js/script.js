121e33ab1a38cd5077bd78e5fd627f7827ab6f85
orlagh-sweeney <orlagh16@gmail.com>
2023-01-04 10:25:11 +0000

    Separate questions from displayQuizA function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
-/**const runQuizA = document.getElementsByClassName('btn-quiz-1');
+const runQuizA = document.getElementsByClassName('btn-quiz-1');
 const runQuizB = document.getElementsByClassName('btn-quiz-2');
 const submitBtn = document.getElementsByClassName('submit');
 const nextBtn = document.getElementsByClassName('next');
 const quitBtn = document.getElementsByClassName('btn-quit');
-const quizName = document.getElementsByClassName('quiz-name');
-const questionText = document.getElementsByClassName('question');
+const quizName = document.getElementById('quiz-name');
+const questionText = document.getElementById('question');
 const optionA = document.getElementById('optionA');
 const optionB = document.getElementById('optionB');
 const optionC = document.getElementById('optionC');
-const userScore = document.getElementsByClass('score');
-const userWrong = document.getElementsByClass('incorrect');
-const userTimer = document.getElementsByClass('time');
-*/
+const userScore = document.getElementsByClassName('score');
+const userWrong = document.getElementsByClassName('incorrect');
+const userTimer = document.getElementsByClassName('time');
+
 
 /**
  * Wait for DOM to finished loading before running the game
@@ -68,86 +68,86 @@ function incrementTimer() {
 
 function displayQuizA() {
     
-    document.getElementById('question').textContent = questions.questionText;
-    document.getElementById('optionA').textContent = optionA;
+    document.getElementById('question').textContent = quizAQuestions.questionText;
+    document.getElementById('optionA').textContent = quizAQuestions.optionA;
     document.getElementById('optionB').textContent = optionB;
     document.getElementById('optionC').textContent = optionC;
 
-    
-    let questions = [
-        {
-            questionText: "1. Who was crowned the Formula 1 2022 World Champion?",
-            optionA: "Max Verstappen",
-            optionB: "Lewis Hamilton",
-            optionC: "Charles Leclerc",
-            correctAnswer: "Max Verstappen",
-        },
-        {
-            questionText: "2. Which team had the fastest pitstop in 2022?",
-            optionA: "Redbull",
-            optionB: "McLaren",
-            optionC: "Ferrari",
-            correctAnswer: "McLaren",
-        },
-        {
-            questionText: "3. What was the most DNFs by any one driver?",
-            optionA: "4",
-            optionB: "5",
-            optionC: "6",
-            correctAnswer: "6",
-        },
-        {
-            questionText: "4. Which team won the 2022 Constructors Championship?",
-            optionA: "Mercedes",
-            optionB: "Redbull",
-            optionC: "Ferrari",
-            correctAnswer: "Redbull",
-        },
-        {
-            questionText: "5. Where was the final Grand Prix of 2022",
-            optionA: "Abu Dhabi",
-            optionB: "Bahrain",
-            optionC: "Brazil",
-            correctAnswer: "Bahrain",
-        },
-        {
-            questionText: "6. Who won the overall 2022 DHL Fastest Lap award?",
-            optionA: "Charles Leclerc",
-            optionB: "Max Verstappen",
-            optionC: "George Russell",
-            correctAnswer: "Max Verstappen",
-        },
-        {
-            questionText: "7. Who was the youngest f1 driver in 2022?",
-            optionA: "Yuki Tsunoda",
-            optionB: "Lando Norris",
-            optionC: "Fernando Alonso",
-            correctAnswer: "Yuki Tsunoda",
-        },
-        {
-            questionText: "8. Which nationality was represented by the most drivers?",
-            optionA: "German",
-            optionB: "Spanish",
-            optionC: "British",
-            correctAnswer: "British",
-        },
-        {
-            questionText: "9. How many World Champions were on the grid in 2022?",
-            optionA: "2",
-            optionB: "4",
-            optionC: "6",
-            correctAnswer: "4",
-        },
-        {
-            questionText: "10. Outside of the 3 top teams (Redbull, Ferrari & Mercedes), who was the only other driver to reach the podium?",
-            optionA: "Lando Norris",
-            optionB: "Fernando Alonso",
-            optionC: "Kevin Magnussen",
-            correctAnswer: "Lando Norris",
-        },
-    ];
 }
 
 function displayQuizB() {
 
 }
+
+let quizAQuestions = [
+    {
+        questionText: "1. Who was crowned the Formula 1 2022 World Champion?",
+        optionA: "Max Verstappen",
+        optionB: "Lewis Hamilton",
+        optionC: "Charles Leclerc",
+        correctAnswer: "Max Verstappen",
+    },
+    {
+        questionText: "2. Which team had the fastest pitstop in 2022?",
+        optionA: "Redbull",
+        optionB: "McLaren",
+        optionC: "Ferrari",
+        correctAnswer: "McLaren",
+    },
+    {
+        questionText: "3. What was the most DNFs by any one driver?",
+        optionA: "4",
+        optionB: "5",
+        optionC: "6",
+        correctAnswer: "6",
+    },
+    {
+        questionText: "4. Which team won the 2022 Constructors Championship?",
+        optionA: "Mercedes",
+        optionB: "Redbull",
+        optionC: "Ferrari",
+        correctAnswer: "Redbull",
+    },
+    {
+        questionText: "5. Where was the final Grand Prix of 2022",
+        optionA: "Abu Dhabi",
+        optionB: "Bahrain",
+        optionC: "Brazil",
+        correctAnswer: "Bahrain",
+    },
+    {
+        questionText: "6. Who won the overall 2022 DHL Fastest Lap award?",
+        optionA: "Charles Leclerc",
+        optionB: "Max Verstappen",
+        optionC: "George Russell",
+        correctAnswer: "Max Verstappen",
+    },
+    {
+        questionText: "7. Who was the youngest f1 driver in 2022?",
+        optionA: "Yuki Tsunoda",
+        optionB: "Lando Norris",
+        optionC: "Fernando Alonso",
+        correctAnswer: "Yuki Tsunoda",
+    },
+    {
+        questionText: "8. Which nationality was represented by the most drivers?",
+        optionA: "German",
+        optionB: "Spanish",
+        optionC: "British",
+        correctAnswer: "British",
+    },
+    {
+        questionText: "9. How many World Champions were on the grid in 2022?",
+        optionA: "2",
+        optionB: "4",
+        optionC: "6",
+        correctAnswer: "4",
+    },
+    {
+        questionText: "10. Outside of the 3 top teams (Redbull, Ferrari & Mercedes), who was the only other driver to reach the podium?",
+        optionA: "Lando Norris",
+        optionB: "Fernando Alonso",
+        optionC: "Kevin Magnussen",
+        correctAnswer: "Lando Norris",
+    },
+];
